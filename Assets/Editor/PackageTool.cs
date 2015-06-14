9cee5f096fbaa82744bbe7572182b460276838f5
Keijiro Takahashi <keijiro@ZX.local>
2015-06-14 23:05:26 +0900

    Added package tool.

---
+++ Assets/Editor/PackageTool.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class PackageTool
+{
+    [MenuItem("Package/Update Package")]
+    static void UpdatePackage()
+    {
+        AssetDatabase.ExportPackage("Assets/Kvant", "KvantSpray.unitypackage", ExportPackageOptions.Recurse);
+    }
+}
