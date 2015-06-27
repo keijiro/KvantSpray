18009a4c7204fe971f4842bb697a71dd405007c0
Keijiro Takahashi <keijiro@ZX.local>
2015-06-27 16:36:27 +0900

    Added a test for material instantiation.

---
+++ Assets/Test/MaterialModifier.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MaterialModifier : MonoBehaviour
+{
+    public Material material;
+
+    IEnumerator Start()
+    {
+        var spray = GetComponent<Kvant.Spray>();
+        while (true)
+        {
+            spray.material.color = Color.red;
+            yield return new WaitForSeconds(1);
+
+            spray.material.color = Color.blue;
+            yield return new WaitForSeconds(1);
+
+            spray.material = material;
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
